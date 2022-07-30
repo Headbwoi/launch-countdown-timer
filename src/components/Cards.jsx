2e38223ab431b96aca1082b28af1680e4418deb7
headbwoi <tochukwujohn24@gmail.com>
2022-07-30 14:20:42 +0100

    finished logic

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -8,7 +8,7 @@ const Cards = (props) => {
           <span className="block absolute top-1/2 right-0 -translate-y-1/2 w-1.5 h-3 bg-black_blue rounded-l-full"></span>
           <span className="block absolute top-1/2 right-0 -translate-y-1/2 w-full h-0.5 bg-black_blue/20"></span>
           <p className="text-Soft_red text-[28px] md:text-[4.75rem] font-bold font-redHat pl-0.5">
-            {props.count.days}
+            {props.count.days.toString().padStart(2, "0")}
           </p>
         </div>
         <p className="text-center uppercase text-[9px] md:text-xs text-Grayish_blue font-bold font-redHat tracking-[3.75px] md:tracking-[7px]">
@@ -23,7 +23,7 @@ const Cards = (props) => {
           <span className="block absolute top-1/2 right-0 -translate-y-1/2 w-1.5 h-3 bg-black_blue rounded-l-full"></span>
           <span className="block absolute top-1/2 right-0 -translate-y-1/2 w-full h-0.5 bg-black_blue/20"></span>
           <p className="text-Soft_red text-[28px] md:text-[4.75rem] font-bold font-redHat pl-0.5">
-            {props.count.hours}
+            {props.count.hours.toString().padStart(2, "0")}
           </p>
         </div>
         <p className="text-center uppercase text-[9px]  md:text-xs text-Grayish_blue font-bold font-redHat tracking-[3.75px] md:tracking-[7px] ">
@@ -38,7 +38,7 @@ const Cards = (props) => {
           <span className="block absolute top-1/2 right-0 -translate-y-1/2 w-1.5 h-3 bg-black_blue rounded-l-full"></span>
           <span className="block absolute top-1/2 right-0 -translate-y-1/2 w-full h-0.5 bg-black_blue/20"></span>
           <p className="text-Soft_red text-[28px] md:text-[4.75rem] font-bold font-redHat pl-0.5">
-            {props.count.minutes}
+            {props.count.minutes.toString().padStart(2, "0")}
           </p>
         </div>
         <p className="text-center uppercase text-[9px] md:text-xs text-Grayish_blue font-bold font-redHat tracking-[3.75px] md:tracking-[7px]">
@@ -53,7 +53,7 @@ const Cards = (props) => {
           <span className="block absolute top-1/2 right-0 -translate-y-1/2 w-1.5 h-3 bg-black_blue rounded-l-full"></span>
           <span className="block absolute top-1/2 right-0 -translate-y-1/2 w-full h-0.5 bg-black_blue/20"></span>
           <p className="text-Soft_red text-[28px] md:text-[4.75rem] font-bold font-redHat pl-0.5">
-            {props.count.seconds}
+            {props.count.seconds.toString().padStart(2, "0")}
           </p>
         </div>
         <p className="text-center uppercase text-[9px] md:text-xs  text-Grayish_blue font-bold font-redHat tracking-[3.75px] md:tracking-[7px]">
