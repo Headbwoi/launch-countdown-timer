42957f410fd50587eb8fc8036b3fcb3852596e48
headbwoi <tochukwujohn24@gmail.com>
2022-07-30 03:07:02 +0100

    configured project setup

---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      // lg: "1115px",
+    },
+    fontFamily: {
+      sans: ["'Red Hat Text', 'sans_serif'"],
+    },
+    colors: {
+      Grayish_blue: "hsl(237, 18%, 59%)",
+      Soft_red: "hsl(345, 95%, 68%)",
+      White: "hsl(0, 0%, 100%)",
+      Dark_desaturated_blue: "hsl(236, 21%, 26%)",
+      Very_dark_blue: "hsl(235, 16%, 14%)",
+      black_blue: "hsl(234, 17%, 12%)",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
