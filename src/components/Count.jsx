7bc6648ba7b3351e64a80d55f57583e78a174d78
headbwoi <tochukwujohn24@gmail.com>
2022-08-15 05:33:56 +0100

    finished

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -5,7 +5,7 @@ import Finished from "./Finished"
 const Count = () => {
   const getDate = () => {
     const presentDate = new Date().getTime()
-    const elapseDate = Date.parse("august 17, 2022")
+    const elapseDate = Date.parse("august 30, 2022")
 
     const dayDivider = 1000 * 24 * 60 * 60
     const hourDivider = 1000 * 60 * 60
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,13 +6,13 @@ function Footer() {
       <span className="block absolute bottom-0 left-0 bg-[url('../images/pattern-hills.svg')] bg-no-repeat bg-cover object-contain h-full w-full "></span>
       {/* icons */}
       <div className="icons absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full flex items-center justify-center space-x-8">
-        <a href="">
+        <a href="https://facebook.com">
           <i className="fab fa-facebook-square text-Grayish_blue text-2xl hover:text-Soft_red cursor-pointer duration-200"></i>
         </a>
-        <a href="">
+        <a href="www.pinterest.com">
           <i className="fab fa-pinterest text-Grayish_blue text-2xl hover:text-Soft_red cursor-pointer duration-200"></i>
         </a>
-        <a href="">
+        <a href="www.instagram.com">
           <i className="fab fa-instagram text-Grayish_blue text-2xl hover:text-Soft_red cursor-pointer duration-200"></i>
         </a>
       </div>
